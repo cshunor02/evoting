8c5dd7af9cf92072923c45c6cf8a18209a1210e0
Kleon Dósa <phmc3s@INF.ELTE.HU>
2025-03-24 16:48:00 +0100

    Adding the missing pages

---
+++ frontend/src/pages/Election.jsx
@@ -0,0 +1,12 @@
+import './../App.css'
+
+const Election = () => {
+    return (
+        <div className="App">
+            <h1>Election Page</h1>
+            <p>Welcome to the election page.</p>
+        </div>
+    );
+};
+
+export default Election;
