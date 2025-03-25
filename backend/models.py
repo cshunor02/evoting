f62b7cd0d012458bd4b94b4748eeabd1cc7e1bbc
Fidan-Sadig <108131533+Fidan-Sadig@users.noreply.github.com>
2025-03-25 01:13:15 +0100

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -11,6 +11,8 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    job = db.Column(db.String(100), nullable=True)  
+
 
     def __repr__(self):
         return f"<User {self.username}>"
