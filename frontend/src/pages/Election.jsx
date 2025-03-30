24aca4f4f2dcd1893d1a87550aaf21e2ac1b2e36
David Benei <beneidavid.it@gmail.com>
2025-03-30 01:43:03 +0100

    Add temporary API verification output

--- frontend/src/pages/Election.jsx
+++ frontend/src/pages/Election.jsx
@@ -2,18 +2,25 @@ import { useEffect, useState } from 'react';
 import './../App.css'
 import { useParams } from 'react-router-dom';
 import FadeIn from 'react-fade-in';
+import useCandidates from '../hooks/useCandidates';
 
 function Election() {
     const params = useParams();
     const id = params.id;
     const [defaultMessage, changeMessage] = useState('HELO')
+    const { 
+        candidates, 
+        fetchCandidates,
+      } = useCandidates();
 
     useEffect(() => {
+        fetchCandidates();
+
         if (id != undefined)
         {
             changeMessage(e => 'This is election ID: ' + id)
         }
-      }, [id]);
+      }, [id,  fetchCandidates]);
 
     return (
         <div className="App">
@@ -21,7 +28,16 @@ function Election() {
                 <h1>Election Page</h1>
                 <p>Welcome to the election page.</p>
                 <p>{defaultMessage}</p>
-            </FadeIn>
+ 
+                <h2>Candidates</h2>
+                <ul>
+                    {candidates.map(candidate => (
+                    <li key={candidate.id}>
+                        {candidate.name}
+                    </li>
+                    ))}
+                </ul>
+            </FadeIn>  
         </div>
     );
 };
