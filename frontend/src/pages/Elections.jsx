152f46d62368b003c519679c592cf6fc58d19f5f
Hunor Csapó <104154290+cshunor02@users.noreply.github.com>
2025-05-21 16:43:10 +0200

    Enhance Elections and Results components with styling and data updates

--- frontend/src/pages/Elections.jsx
+++ frontend/src/pages/Elections.jsx
@@ -5,34 +5,6 @@ import { Link } from "react-router-dom";
 import FadeIn from 'react-fade-in';
 
 const defaultData = {
-    '0': {
-        'id' : 0,
-        'title' : 'Presidental Election 2024',
-        'description' : 'This is a description field, where if the description is too long, it overflows.',
-        'start_time' : '2012-04-23T18:25:43.511Z',
-        'end_time' : '2025-05-05T18:25:43.511Z'
-    },
-    '1': {
-        'id' : 1,
-        'title' : 'Local Council Election',
-        'description' : 'This is a description field, where if the description is too long, it overflows. The maximum line numbers should be only 2 lines.',
-        'start_time' : '2025-01-12',
-        'end_time' : '2025-03-23'
-    },
-    '2': {
-        'id' : 2,
-        'title' : 'High School Kahoot',
-        'description' : 'This is a description field, where if the description is too long, it overflows.',
-        'start_time' : '2025-01-12',
-        'end_time' : '2025-05-05'
-    },
-    '3': {
-        'id' : 3,
-        'title' : 'G0ogl3 F0rm5',
-        'description' : 'This is a description field, where if the description is too long, it overflows.',
-        'start_time' : '2025-01-12',
-        'end_time' : '2025-05-05'
-    }
 }
 
 function Elections() {
